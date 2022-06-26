4ce718d77c91201a97f147171f10a8143bdb1f0b
Alexander Shen <70786647+shenalexw@users.noreply.github.com>
2022-06-25 17:27:09 -0700

    test index

--- coffee.py
+++ coffee.py
@@ -6,17 +6,17 @@ from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
 # Load dotenv
-env_path = Path('.')/'.env'
-load_dotenv(dotenv_path=env_path)
+# env_path = Path('.')/'.env'
+# load_dotenv(dotenv_path=env_path)
 
 # init flask and slack event adapter
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET'], '/slack/events', app)
+# slack_event_adapter = SlackEventAdapter(
+#     os.environ['SIGNING_SECRET'], '/slack/events', app)
 
 # init client and bot_id
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-BOT_ID = client.api_call("auth.test")["user_id"]
+# client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+# BOT_ID = client.api_call("auth.test")["user_id"]
 
 
 # Handles messages from anyone in the channel the bot is in
@@ -37,42 +37,42 @@ def index():
 # Handles Tally
 
 
-@app.route('/tally', methods=['POST'])
-def tally():
-    data = request.form
-    channel_id = data.get('channel_id')
-    user_id = data.get('user_id')
-    user_name = data.get('user_name')
-    text = data.get('text')
-    client.chat_postMessage(
-        channel=channel_id, text=f"Hi {user_name}, Got your /tally for {text}")
-    return Response(), 200
+# @app.route('/tally', methods=['POST'])
+# def tally():
+#     data = request.form
+#     channel_id = data.get('channel_id')
+#     user_id = data.get('user_id')
+#     user_name = data.get('user_name')
+#     text = data.get('text')
+#     client.chat_postMessage(
+#         channel=channel_id, text=f"Hi {user_name}, Got your /tally for {text}")
+#     return Response(), 200
 
-# Handles reset-tally
-
-
-@app.route('/reset-tally', methods=['POST'])
-def resetTally():
-    data = request.form
-    channel_id = data.get('channel_id')
-    user_id = data.get('user_id')
-    user_name = data.get('user_name')
-    client.chat_postMessage(
-        channel=channel_id, text=f"Hi {user_name}, resetting your tally for today")
-    return Response(), 200
-
-# Handles Scoreboard
+# # Handles reset-tally
 
 
-@app.route('/scoreboard', methods=['POST'])
-def scoreboard():
-    data = request.form
-    channel_id = data.get('channel_id')
-    user_id = data.get('user_id')
-    user_name = data.get('user_name')
-    client.chat_postMessage(
-        channel=channel_id, text=f"Hi {user_name}, Here is your scoreboard")
-    return Response(), 200
+# @app.route('/reset-tally', methods=['POST'])
+# def resetTally():
+#     data = request.form
+#     channel_id = data.get('channel_id')
+#     user_id = data.get('user_id')
+#     user_name = data.get('user_name')
+#     client.chat_postMessage(
+#         channel=channel_id, text=f"Hi {user_name}, resetting your tally for today")
+#     return Response(), 200
+
+# # Handles Scoreboard
+
+
+# @app.route('/scoreboard', methods=['POST'])
+# def scoreboard():
+#     data = request.form
+#     channel_id = data.get('channel_id')
+#     user_id = data.get('user_id')
+#     user_name = data.get('user_name')
+#     client.chat_postMessage(
+#         channel=channel_id, text=f"Hi {user_name}, Here is your scoreboard")
+#     return Response(), 200
 
 
 if __name__ == "__main__":
